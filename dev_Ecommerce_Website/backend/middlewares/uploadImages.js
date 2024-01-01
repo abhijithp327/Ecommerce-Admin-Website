2d79899ad94acfc05acbbbea80073df95a7b19c8
Abhijith P <87142203+abhijithp327@users.noreply.github.com>
2024-01-01 14:38:12 +0530

    Update uploadImages.js

--- dev_Ecommerce_Website/backend/middlewares/uploadImages.js
+++ dev_Ecommerce_Website/backend/middlewares/uploadImages.js
@@ -10,8 +10,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, path.join(__dirname, "../public/images/"));
